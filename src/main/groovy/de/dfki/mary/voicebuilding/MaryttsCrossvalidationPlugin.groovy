71236c93e48156da4e62a4a97c7dc97707800980
Insa Kröger <insa.kroeger@outlook.com>
2017-09-29 16:21:21 +0200

    generalize plugin voice dependency

--- src/main/groovy/de/dfki/mary/voicebuilding/MaryttsCrossvalidationPlugin.groovy
+++ src/main/groovy/de/dfki/mary/voicebuilding/MaryttsCrossvalidationPlugin.groovy
@@ -15,15 +15,15 @@ class MaryttsCrossvalidationPlugin implements Plugin<Project> {
             jcenter()
             flatDir {
                 dirs "$project.buildDir/libs"
-                dirs "$project.buildDir/mary/voice-hvoice-test/build/libs"
+//                dirs "$project.buildDir/mary/voice-${project.name}/build/libs"
             }
         }
         project.dependencies {
             marytts 'de.dfki.mary:marytts-voicebuilding:0.1'
             marytts 'de.dfki.mary:marytts-lang-en:5.2'
             marytts 'de.dfki.mary:marytts-runtime:5.2'
-//            marytts 'de.dfki.mary:hvoice-test-0.5.0-SNAPSHOT'
-            marytts 'de.dfki.mary:voice-hvoice-test'
+            marytts "${project.group}:${project.name}:${project.version}"
+//            marytts "${project.group}:voice-${project.name}"
         }
         project.task('selectCrossvalidationFiles', type: SelectCrossvalidationFiles) {
         }
@@ -36,11 +36,11 @@ class MaryttsCrossvalidationPlugin implements Plugin<Project> {
         project.task('getRealisedDurations', type: GetRealisedDurations) {
             dependsOn project.synthesizeCrossvalidationAudio
         }
-        project.task('runAnalysis', type: RunAnalysis) {
-//            dependsOn project.getRealisedDurations
-        }
-//        project.task('runCrossvalidation', type: RunCrossvalidation) {
-//            dependsOn project.getRealisedDurations
+//        project.task('runAnalysis', type: RunAnalysis) {
+////            dependsOn project.getRealisedDurations
 //        }
+        project.task('runCrossvalidation', type: RunCrossvalidation) {
+            dependsOn project.getRealisedDurations
+        }
     }
 }

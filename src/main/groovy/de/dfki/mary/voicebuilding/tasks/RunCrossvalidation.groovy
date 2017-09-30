1b1c6f60477eaefb9c2e46490d681debb8dab3f0
Insa Kröger <insa.kroeger@outlook.com>
2017-09-30 18:19:13 +0200

    redirect output of RunCrossvalidation.groovy to file

--- src/main/groovy/de/dfki/mary/voicebuilding/tasks/RunCrossvalidation.groovy
+++ src/main/groovy/de/dfki/mary/voicebuilding/tasks/RunCrossvalidation.groovy
@@ -3,6 +3,7 @@ package de.dfki.mary.voicebuilding.tasks
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.*
 import marytts.*
+import java.io.ByteArrayOutputStream
 
 class RunCrossvalidation extends DefaultTask {
 
@@ -18,6 +19,9 @@ class RunCrossvalidation extends DefaultTask {
     @OutputFile
     File allophonesFile = project.file("$project.buildDir/crossvalidation/allophones_en_US.xml")
 
+    @OutputFile
+    File outputFile = project.file("$project.buildDir/crossvalidation/rmslsfdistortion-results.txt")
+
     @Input
     Map<String, String> maryttsProperties = ['mary.base': "$project.buildDir/resources/legacy"]
 
@@ -26,13 +30,18 @@ class RunCrossvalidation extends DefaultTask {
         def allophonesString = this.class.getResourceAsStream('/de/dfki/mary/voicebuilding/templates/allophones_en_US.xml').text
         if (allophonesFile.length()==0) {
             allophonesFile << allophonesString }
+
+        def stdout = new ByteArrayOutputStream()
+
         project.javaexec {
             classpath project.configurations.marytts
             main 'marytts.signalproc.analysis.distance.RmsLsfDistortionComputer'
             maryttsProperties.put('allophoneset', "$allophonesFile.path")
             systemProperties << maryttsProperties
             args refDir, srcDir
+            standardOutput = stdout
         }
+        outputFile.text = stdout
     }
 
 }
